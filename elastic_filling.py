66b33b091ce616a404c79d695a524d6922bd1d2e
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-02 16:33:18 +0300

    Вынес логику работу с elasticsearch в один файл

--- elastic_filling.py
+++ elastic_filling.py
@@ -1,42 +1,40 @@
-import os
-
-from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
-from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 from models import Document
 
-from config import client
-from dotenv import load_dotenv
+from config import client, db_engine
 
-
-# ==== Подключение к БД и Elasticsearch ====
-load_dotenv()
-user = os.getenv("DB_USER")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-database = os.getenv("DB_NAME")
-
-engine = create_engine(f"postgresql+psycopg://{user}:{password}@{host}/{database}")
 
 # ==== Выгрузка данных из БД в индекс ====
 # создание сессии и загрузка данных в БД
 
-try:
-    with Session(bind=engine) as db:
-        documents = db.query(Document).all()
-        total = len(documents)
-        documents_dict = [{"_index": "documents", "_id": doc.id, "_source": {"id": doc.id, "text": doc.text}} for doc in documents]
-        
-        success, failed = bulk(client, documents_dict)
-        print(f"Успешно: {success}, Failed: {len(failed)}")
-        
-        print(f"Всего документов: {total}")
+def fill_elastic():
+    # удаление индекса если есть
+    if client.indices.exists(index="documents"):
+        client.indices.delete(index="documents")
+
+    # создание нового индекса
+    mappings = {
+        "properties": {
+            "id": {"type": "integer"},
+            "text": {"type": "text"}
+        }
+    }
+
+    client.indices.create(index="documents", mappings=mappings)
+
+    try:
+        with Session(bind=db_engine) as db:
+            documents = db.query(Document).all()
+            total = len(documents)
+            documents_dict = [{"_index": "documents", "_id": doc.id, "_source": {"id": doc.id, "text": doc.text}} for doc in documents]
             
-except Exception as e:
-    print(f"Возникла ошибка: {e}")
+            success, failed = bulk(client, documents_dict)
+            print(f"Успешно: {success}, Failed: {len(failed)}, Всего: {total}")
+                
+    except Exception as e:
+        print(f"Возникла ошибка: {e}")
 
-def delete_index():
-    client.indices.delete(index="documents")
 
-
+if __name__ == "__main__":
+    fill_elastic()
