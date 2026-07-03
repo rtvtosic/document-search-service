5da8b7f57aa583c0983134d01f57d4c2c08cadf4
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-03 18:20:39 +0300

    Сделан интерфейс для поисковика документов

--- ui.py
+++ ui.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import streamlit as st
 
 
-
 API_URL = "http://localhost:8000"
 
 st.title("Поиск по документам")
@@ -17,19 +16,29 @@ if st.button("Поиск"):
         )
 
         if response.status_code == 200:
-            results = response.json()
-            st.write(f"**Найдено документов:** {len(results)}")
-
-            # вывод каждого документа
-            for doc in results:
-                    st.header(f"#{doc['id']}",
-                              divider="violet")
-                    
-                    st.write(doc["text"])
-                    date_obj = datetime.fromisoformat(doc['created_date'])
-                    st.caption(f"{date_obj.hour}:{date_obj.minute} {date_obj.day}.{date_obj.month}.{date_obj.year}")
-                    st.caption(f"Рубрики: {', '.join(doc["rubrics"])}")
+            st.session_state["results"] = response.json()
         else:
             st.error(f"Ошибка: {response.status_code}")
     else:
         st.warning("Введите запрос")
+
+if "results" in st.session_state:
+    for doc in st.session_state["results"]:
+        # header, text, caption
+        st.header(f"#{doc['id']}", divider="violet")       
+        st.write(doc["text"])
+        date_obj = datetime.fromisoformat(doc['created_date'])
+        st.caption(f"{date_obj.hour}:{date_obj.minute} {date_obj.day}.{date_obj.month}.{date_obj.year}")
+        st.caption(f"Рубрики: {', '.join(doc["rubrics"])}")
+
+        if st.button("Удалить", key=f"del_{doc['id']}"):
+            del_response = requests.delete(f"{API_URL}/documents/{doc['id']}")
+
+            if del_response.status_code == 200:
+                st.session_state["results"] = [
+                    d for d in st.session_state["results"] if d["id"] != doc["id"]
+                ]
+                
+                st.rerun()
+            else:
+                st.error("Не удалось удалить")
