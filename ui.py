a57d3b2d14f40505f826b0b23887576779e00cf8
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-06 20:20:08 +0300

    Рефакторинг файла ui.py

--- ui.py
+++ ui.py
@@ -1,8 +1,10 @@
+"""Интерфейс сервиса"""
+
 import os
+from datetime import datetime
+
 import requests
 import streamlit as st
-
-from datetime import datetime
 
 
 API_URL = os.getenv("API_URL", "http://localhost:8000")
@@ -14,7 +16,8 @@ query = st.text_input("Введите поисковой запрос")
 if st.button("Поиск"):
     if query:
         response = requests.post(
-            f"{API_URL}/search", json={"query": query}
+            f"{API_URL}/search", json={"query": query},
+            timeout=5
         )
 
         if response.status_code == 200:
@@ -28,8 +31,12 @@ if "results" in st.session_state:
     for doc in st.session_state["results"]:
         st.header(f"#{doc['id']}", divider="violet")
         st.write(doc["text"])
+
         date_obj = datetime.fromisoformat(doc['created_date'])
-        st.caption(f"{date_obj.hour}:{date_obj.minute} {date_obj.day}.{date_obj.month}.{date_obj.year}")
+        hr_min = f"{date_obj.hour}:{date_obj.minute}"
+        date = f"{date_obj.day}.{date_obj.month}.{date_obj.year}"
+
+        st.caption(f"{hr_min} {date}")
         st.caption(f"Рубрики: {', '.join(doc['rubrics'])}")
 
         # если подтверждение запрошено ИМЕННО для этого документа — показываем вопрос
@@ -38,7 +45,8 @@ if "results" in st.session_state:
             col1, col2 = st.columns(2)
             with col1:
                 if st.button("Да, удалить", key=f"yes_{doc['id']}"):
-                    del_response = requests.delete(f"{API_URL}/documents/{doc['id']}")
+                    del_response = requests.delete(f"{API_URL}/documents/{doc['id']}",
+                                                   timeout=5)
                     if del_response.status_code == 200:
                         st.session_state["results"] = [
                             d for d in st.session_state["results"] if d["id"] != doc["id"]
