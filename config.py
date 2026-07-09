80285887100598105d0e499b3d31159ad7eecbf5
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-09 20:35:49 +0300

    Добавлено асинхронное подключение к Postgres и Elasticsearch

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 
 import os
 
-from sqlalchemy import create_engine
-from elasticsearch import Elasticsearch
+from sqlalchemy.ext.asyncio import create_async_engine
+from elasticsearch import AsyncElasticsearch
 from dotenv import load_dotenv
 
 
@@ -17,7 +17,7 @@ database = os.getenv("DB_NAME")
 elastic_host = os.getenv("ELASTIC_HOST")
 elastic_port = os.getenv("ELASTIC_PORT")
 
-db_engine = create_engine(f"postgresql+psycopg://{user}:{password}@{host}/{database}")
-client = Elasticsearch(
+db_engine = create_async_engine(f"postgresql+psycopg://{user}:{password}@{host}/{database}")
+client = AsyncElasticsearch(
     hosts=[f"http://{elastic_host}:{elastic_port}"]
 )
