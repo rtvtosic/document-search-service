c107696bb5bebf74f8bf8c61588e0a51ebabca57
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-19 15:59:22 +0300

    Исправлены импорты в elastic_filling

--- scripts/elastic_filling.py
+++ scripts/elastic_filling.py
@@ -2,7 +2,7 @@ from elasticsearch.helpers import bulk
 from sqlalchemy.orm import Session
 from models import Document
 
-from config import client, db_engine
+from config import sync_client, sync_db_engine
 
 
 # ==== Выгрузка данных из БД в индекс ====
@@ -10,8 +10,8 @@ from config import client, db_engine
 
 def fill_elastic():
     # удаление индекса если есть
-    if client.indices.exists(index="documents"):
-        client.indices.delete(index="documents")
+    if sync_client.indices.exists(index="documents"):
+        sync_client.indices.delete(index="documents")
 
     # создание нового индекса
     mappings = {
@@ -21,15 +21,15 @@ def fill_elastic():
         }
     }
 
-    client.indices.create(index="documents", mappings=mappings)
+    sync_client.indices.create(index="documents", mappings=mappings)
 
     try:
-        with Session(bind=db_engine) as db:
+        with Session(bind=sync_db_engine) as db:
             documents = db.query(Document).all()
             total = len(documents)
             documents_dict = [{"_index": "documents", "_id": doc.id, "_source": {"id": doc.id, "text": doc.text}} for doc in documents]
             
-            success, failed = bulk(client, documents_dict)
+            success, failed = bulk(sync_client, documents_dict)
             print(f"Успешно: {success}, Failed: {len(failed)}, Всего: {total}")
                 
     except Exception as e:
