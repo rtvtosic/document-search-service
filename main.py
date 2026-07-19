51f9c913dbd0e4abfab89a3a2f3d9b2905a61eb6
cactus3106@gmail.com <anton3105@bk.ru>
2026-07-19 17:36:27 +0300

    асинхронные эндпоинты

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 """Главный файл проекта с реализацией эндпоинтов"""
+
 import uvicorn
 
 from elasticsearch import NotFoundError
 from fastapi import FastAPI, HTTPException, Depends
 
-from sqlalchemy import desc
-from sqlalchemy.orm import Session
+from sqlalchemy import desc, select
+from sqlalchemy.ext.asyncio import AsyncSession
 from models import Document
 
 from database import get_db
-from config import sync_client
+from config import async_client
 
 from schemas import SearchRequest, DocumentSchema
 
@@ -23,15 +24,15 @@ app = FastAPI(
         response_model=list[DocumentSchema],
         tags=["Методы"],
         summary="Поиск документа по тексту")
-def search_docs_by_text(request: SearchRequest,
-                        db: Session = Depends(get_db)):
+async def search_docs_by_text(request: SearchRequest,
+                        db: AsyncSession = Depends(get_db)):
     """Поиск документа по тексту"""
 
     # ==== Поиск нужных id в индексе в Elasticsearch ====
     document_ids = []
     result_documents = []
 
-    resp = sync_client.search(
+    resp = await async_client.search(
         index="documents",
         query={
             "match": {
@@ -44,35 +45,36 @@ def search_docs_by_text(request: SearchRequest,
     for doc in resp['hits']['hits']:
         document_ids.append(doc['_source']['id'])
 
-    # ==== Поиск данных по id в Postgres ====
-    query = db.query(Document).filter(
-        Document.id.in_(document_ids)
-    ).order_by(desc(Document.created_date)).all()
+    result = await db.execute(
+        select(Document).where(Document.id.in_(document_ids)).order_by(desc(Document.created_date))
+    )
 
-    for doc in query:
-        result_documents.append(doc)
+    documents = result.scalars().all()
 
-    return result_documents
+    # for doc in documents:
+    #     result_documents.append(doc)
+
+    return documents
 
 # удалять документ из БД и индекса по полю id.
 @app.delete("/documents/{doc_id}",
             tags=["Методы"],
             summary="Удаление документа из БД и индекса по id")
-def delete_doc_by_id(doc_id: int, db: Session = Depends(get_db)):
+async def delete_doc_by_id(doc_id: int, db: AsyncSession = Depends(get_db)):
     """Удаление документа из БД и индекса по полю id"""
 
     # удаление из Postgres
-    doc_to_delete = db.get(Document, doc_id)
+    doc_to_delete = await db.get(Document, doc_id)
 
     if doc_to_delete is None:
         raise HTTPException(status_code=404, detail="Document Not Found")
 
-    db.delete(doc_to_delete)
-    db.commit()
+    await db.delete(doc_to_delete)
+    await db.commit()
 
     # удаление из Elasticsearch
     try:
-        sync_client.delete(index="documents", id=str(doc_id))
+        await async_client.delete(index="documents", id=str(doc_id))
     except NotFoundError:
         pass
 
